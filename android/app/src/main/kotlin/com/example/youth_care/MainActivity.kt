58c2842c6e0da6b62bfbf021e2581753bf736e2f
mohamedmamdouh22 <muhammed195896@feng.bu.edu.eg>
2022-10-10 21:50:03 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/example/youth_care/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.youth_care
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(YouthCare());
+}
+class YouthCare extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      routes: {
+
+      },
+      
+    );
+  }
+
+}
